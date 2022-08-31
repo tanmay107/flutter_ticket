58e31bdc8a695e85de37b58ae9a435ed4f0f9d62
TanmayTripathi <tanmaytripathi@InstantSystemss-MacBook-Pro-4.local>
2022-08-31 13:46:47 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_booking/screens/bottom_bar.dart';
+import 'package:ticket_booking/utils/app_styles.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: primary,
+      ),
+      home: const BottomBar(),
+    );
+  }
+}
